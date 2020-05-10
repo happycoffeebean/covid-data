65f42f7c8688225450662c7415fddccbc9d619c5
Lisa MacNeil <8798898+happycoffeebean@users.noreply.github.com>
2020-05-10 10:59:58 -0400

    Add files via upload

---
+++ stats.php
@@ -0,0 +1,47 @@
+<!doctype html>
+<html lang="en">
+<?php $debug = FALSE; ?>
+
+<head>
+    <title>Hernando County's latest COVID stats</title>
+    <link rel="stylesheet" type="text/css" href="">
+    
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="keywords" content="hernando, hernando sun, newspaper, COVID">
+    <meta name="author" content="happycoffeebean, t137">
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+</head>
+
+  <body>
+    <h1>Hernando County Daily COVID stats</h1>
+    
+    <?php
+    exec('wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv',$dump ,$err_code);
+    
+    if ($debug == TRUE) {
+        if($err_code == '0') {
+            echo "Well done";
+        }  else {
+            echo "You screwed something up. Errorcode: $err_code";
+        }
+    }
+
+    $delim = ',';
+    $fp = fopen('us-counties.csv', 'r');
+    
+    echo "Date    |  Cases   |  Deaths" ."<br><br>";  
+    while(($row = fgetcsv($fp, 1024, $delim)) !== false) {
+        if ($row[1] == 'Hernando'){
+            //print_r($row);
+            // date,county,state,fips,cases,deaths
+            
+            echo "$row[0] |  $row[4] |  $row[5]" . "<br>";
+        } 
+        
+        // print_r($row);
+    }
+    ?>
+</body>
+</html>
